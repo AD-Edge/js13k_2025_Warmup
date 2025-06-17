9a1cd3718e6eec46e8b0bd3ad24d3425153566ed
AD-Edge <alex.delderfield@gmail.com>
2025-06-17 20:21:59 +0930

    working on input setup, with basic canvas output

--- js13k_template_2025_0/dev_codebase/src/game/input.js
+++ js13k_template_2025_0/dev_codebase/src/game/input.js
@@ -1,3 +1,68 @@
 //////////////////////////////////////////////////////////
 //                   Input Setup                        //
 //////////////////////////////////////////////////////////
+// Keys states (false: key is released / true: key is pressed)
+up = right = down = left = false;
+
+// Keydown listener
+onkeydown = (e) => {
+    // Up (up / W / Z)
+    if(e.key == 38 || e.key == 90 || e.key == 87){
+    up = true;
+    }
+    // Right (right / D)
+    if(e.key == 39 || e.key == 68){
+    right = true;
+    }
+    // Down (down / S)
+    if(e.key == 40 || e.key == 83){
+    down = true;
+    }
+    // Left (left / A / Q)
+    if(e.key == 37 || e.key == 65 ||e.key == 81){
+    left = true;
+    }
+
+    console.log("UP: " + up);
+}
+
+// Keyup listener
+onkeyup = (e) => {
+    // Up
+    if(e.key == 38 || e.key == 90 || e.key == 87){
+        up = false;
+    }
+    // Right
+    if(e.key == 39 || e.key == 68){
+        right = false;
+    }
+    // Down
+    if(e.key == 40 || e.key == 83){
+        down = false;
+    }
+    // Left
+    if(e.key == 37 || e.key == 65 || e.key == 81){
+        left = false;
+    }
+}
+
+function setupEventListeners(c) {
+    c.addEventListener('keydown', onkeydown(e));
+
+    // Event listener to track mouse/touch movement
+    c.addEventListener('pointermove', (e) => {
+        console.log("pointermove");
+    });
+    // Event listener to track mouse/touch down
+    c.addEventListener('pointerdown', (e) => {
+        console.log("pointerdown");
+    });
+    // Event listener to track mouse/touch up
+    c.addEventListener('pointerup', (e) => {
+        console.log("pointerup");
+    });
+    // The same as pointer up, but for mobile specific cases
+    c.addEventListener('pointercancel', (e) => {
+        console.log("pointercancel");
+    });
+}
--- js13k_template_2025_0/dev_codebase/src/main.js
+++ js13k_template_2025_0/dev_codebase/src/main.js
@@ -2,6 +2,15 @@
 //                        MAIN                          //
 //////////////////////////////////////////////////////////
 
+var up = false, right = false, down = false, left = false;
+// Baseline variables 
+var mobile, app, cvs, cx, w, h, asp, asp2, rect, rng, seed, currentHover, currentHeld, mouseX, mouseY;
+// Scalings
+var w2 = 960; var h2 = 540;
+// Toggles
+var debug = true;
+var webGL = true;
+
 // App Setup
 window.onload = function() 
 {
@@ -21,6 +30,8 @@ function initSetup()
     
     cx.imageSmoothingEnabled = false;
 
+    setupEventListeners(cvs);
+
     // Mobile/Resizing handling 
 
     // Kick off Loading/Generating Assets
@@ -31,8 +42,75 @@ function initSetup()
 
 function tick(timestamp) 
 {
-    // cx.clearRect(0, 0, w, h);
-    console.log("Tick");
+    // console.log("Tick");
+    cx.clearRect(0, 0, w, h);
+    
+    cx.font = '16px monospace';
+
+    cx.fillStyle = '#fff';
+    if(up) { cx.fillStyle = '#3f3'; }
+    cx.fillText('Up: ' + up, 10, 30);
+    cx.fillStyle = '#fff';
+    if(down) { cx.fillStyle = '#3f3'; }
+    cx.fillText('Down: ' + down, 10, 50);
+    cx.fillStyle = '#fff';
+    if(left) { cx.fillStyle = '#3f3'; }
+    cx.fillText('Left: ' + left, 10, 70);
+    cx.fillStyle = '#fff';
+    if(right) { cx.fillStyle = '#3f3'; }
+    cx.fillText('Right: ' + right, 10, 90);
     // Request next frame, ie r loop
     requestAnimationFrame(tick);
+}
+
+// Keydown listener
+onkeydown = (e) => {
+    const k = e.key.toLowerCase();
+    // Up (up / W / Z)
+    if (k === "arrowup" || k === "w" || k === "z") up = true;
+    // Right (right / D)
+    if (k === "arrowright" || k === "d") right = true;
+    // Down (down / S)
+    if (k === "arrowdown" || k === "s") down = true;
+    // Left (left / A / Q)
+    if (k === "arrowleft" || k === "a" ||k === "q") left = true;
+
+    // console.log("E: " + e.key);
+}
+
+// Keyup listener
+onkeyup = (e) => {
+    const k = e.key.toLowerCase();
+    // Up
+    if (k === "arrowup" || k === "w" || k === "z") up = false;
+    // Right
+    if (k === "arrowright" || k === "d") right = false;
+    // Down
+    if (k === "arrowdown" || k === "s") down = false;
+    // Left
+    if(k === "arrowleft" || k === "a" || k === "q") left = false;
+
+    // console.log("UP: " + up);
+}
+
+function setupEventListeners(c) {
+    c.addEventListener('keydown', onkeydown);
+    c.addEventListener('keyup', onkeyup);
+
+    // Event listener to track mouse/touch movement
+    c.addEventListener('pointermove', (e) => {
+        console.log("pointermove");
+    });
+    // Event listener to track mouse/touch down
+    c.addEventListener('pointerdown', (e) => {
+        console.log("pointerdown");
+    });
+    // Event listener to track mouse/touch up
+    c.addEventListener('pointerup', (e) => {
+        console.log("pointerup");
+    });
+    // The same as pointer up, but for mobile specific cases
+    c.addEventListener('pointercancel', (e) => {
+        console.log("pointercancel");
+    });
 }
