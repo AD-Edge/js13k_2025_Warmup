57cc775c52fa37fbb001d1b5476583c5cf6c099d
AD-Edge <alex.delderfield@gmail.com>
2025-06-18 14:43:22 +0930

    saving v0.2 of input setup

--- js13k_template_2025_0/dev_codebase/src/game/input.js
+++ js13k_template_2025_0/dev_codebase/src/game/input.js
@@ -15,6 +15,7 @@ var p=navigator.getGamepads()[0]
 //120 bytes (whole function)
 onkeydown=n=>{let e=n.key;u|="U"==e[5]|"w"==e|"z"==e,d|="D"==e[5]|"s"==e,l|="L"==e[5]|"a"==e|"q"==e,r|="R"==e[5]|"d"==e}
 
+// ~300 bytes
 // function onKeyDown(e) {
 //     let k=e.key
     
@@ -47,18 +48,35 @@ onkeydown=n=>{let e=n.key;u|="U"==e[5]|"w"==e|"z"==e,d|="D"==e[5]|"s"==e,l|="L"=
 //     // console.log("E: " + e.key);
 // }
 
-function onKeyUp(e) {
-    let k=e.key.toLowerCase()
-    if(k=="arrowup" || k=="w" || k=="z") u=0
-    if(k=="arrowdown" || k=="s") d=0
-    if(k=="arrowleft" || k=="a" || k=="q") l=0
-    if(k=="arrowright" || k=="d") r=0
-    // console.log("E: " + e.key);
-}
+// function onKeyUp(e) {
+//     let k=e.key.toLowerCase()
+//     if(k=="arrowup" || k=="w" || k=="z") u=0
+//     if(k=="arrowdown" || k=="s") d=0
+//     if(k=="arrowleft" || k=="a" || k=="q") l=0
+//     if(k=="arrowright" || k=="d") r=0
+//     // console.log("E: " + e.key);
+// }
+
+// onkeyup = e => {
+//     let k = e.key;
+//     if("U"==k[5]||"w"==k||"z"==k)u=0;
+//     if("D"==k[5]||"s"==k)d=0;
+//     if("L"==k[5]||"a"==k||"q"==k)l=0;
+//     if("R"==k[5]||"d"==k)r=0;
+// };
+
+// onkeyup = e => {
+//     let k = e.key;
+//     u&=!("U"==k[5]|"w"==k|"z"==k),
+//     d&=!("D"==k[5]|"s"==k),
+//     l&=!("L"==k[5]|"a"==k|"q"==k),
+//     r&=!("R"==k[5]|"d"==k);
+// };
+onkeyup=e=>{let k=e.key;u&=!("U"==k[5]|"w"==k|"z"==k),d&=!("D"==k[5]|"s"==k),l&=!("L"==k[5]|"a"==k|"q"==k),r&=!("R"==k[5]|"d"==k)};
 
 export function setupEventListeners(c) {
     window.addEventListener('keydown', onkeydown);
-    window.addEventListener('keyup', onKeyUp);
+    window.addEventListener('keyup', onkeyup);
 
     window.addEventListener("gamepadconnected", (e) => {
         console.log("Gamepad connected at index %d: %s. %d buttons, %d axes.",
